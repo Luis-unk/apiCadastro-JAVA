01c3972e781fa31d8e404051ee49ac5e873e3cb9
Luis-unk <santosluiss2022@gmail.com>
2024-05-08 08:18:10 -0300

    security - criptografia nas senhas

--- projetoJAVA/src/main/java/br/com/criandoapi/projetoJAVA/ProjetoJavaApplication.java
+++ projetoJAVA/src/main/java/br/com/criandoapi/projetoJAVA/ProjetoJavaApplication.java
@@ -2,8 +2,9 @@ package br.com.criandoapi.projetoJAVA;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class ProjetoJavaApplication {
 
 	public static void main(String[] args) {
--- projetoJAVA/src/main/java/br/com/criandoapi/projetoJAVA/service/UsuarioService.java
+++ projetoJAVA/src/main/java/br/com/criandoapi/projetoJAVA/service/UsuarioService.java
@@ -2,9 +2,10 @@ package br.com.criandoapi.projetoJAVA.service;
 
 import br.com.criandoapi.projetoJAVA.model.Usuario;
 import br.com.criandoapi.projetoJAVA.repository.IUsuario;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
+
 
 import java.util.List;
 
@@ -12,9 +13,11 @@ import java.util.List;
 public class UsuarioService {
 
     private IUsuario repository;
+    private PasswordEncoder passwordEncoder;
 
     public UsuarioService(IUsuario repository) {
         this.repository = repository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
     public List<Usuario> listarUsuario(){
@@ -23,11 +26,15 @@ public class UsuarioService {
     }
 
     public Usuario criarUsuario(Usuario usuario) {
+        String encoder = this.passwordEncoder.encode(usuario.getSenha());
+        usuario.setSenha(encoder);
         Usuario novoUsuario = repository.save(usuario);
         return novoUsuario;
     }
 
     public Usuario editarUsuario(Usuario usuario) {
+        String encoder = this.passwordEncoder.encode(usuario.getSenha());
+        usuario.setSenha(encoder);
         Usuario updateUsuario = repository.save(usuario);
         return updateUsuario;
     }
@@ -38,4 +45,9 @@ public class UsuarioService {
     }
 
 
+    public Boolean validarSenha(Usuario usuario) {
+        String senha = repository.getById(usuario.getId()).getSenha();
+        Boolean valid = passwordEncoder.matches(usuario.getSenha(), senha);
+        return valid;
+    }
 }
